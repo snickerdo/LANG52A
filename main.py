28b2cbdf1f4183552a14d4f3fd1948543bb9357b
snickerdo <143821167+snickerdo@users.noreply.github.com>
2023-09-06 14:13:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+length = float(input("Enter the length:"))
+width = float(input("Enter the width:"))
+
+area = length * width
+prem = 2 * (length + width)
+
+print(f"The area is {area}")
+print(f"The perimeter is {prem}")
